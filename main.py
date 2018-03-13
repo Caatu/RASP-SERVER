9af6a756bcc6fa12b058a2b9a14e98e60ccdec3e
Gustavo de Oliveira Guerino <gustavoguerino@hotmail.com>
2018-03-13 13:53:39 -0300

    Corrigido loop infinito na conexão

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
 from callbacks import *
 
+
 def initializeConnection(username, password, client_id, broker, port):
     """
         Create client object and initialize the connection with mqtt server
@@ -18,17 +19,17 @@ def initializeConnection(username, password, client_id, broker, port):
         client_id=client_id, 
         clean_session=False,  
     )
-    client.username_pw_set(username=username, password=password)
-
     client.on_connect   =   on_connect
     client.on_log       =   on_log
     client.on_message   =   on_message
+    client.username_pw_set(username=username, password=password)
     print("Connecting to broker")
     try:
         client.connect(broker,port=port)
     except:
         print("Connection failed")
         client.bad_connection_flag = True
+    client.loop_start()
     # Wait to connection success or error occur
     while not client.connected_flag and not client.bad_connection_flag:
         print("In wait loop")
@@ -37,7 +38,6 @@ def initializeConnection(username, password, client_id, broker, port):
         # When occur error in connection stop the program
         # TODO: Filter possibles errors in on_conect and print this on log file
         finish()
-    client.loop_start()
     print("In Main Loop")
 
 
